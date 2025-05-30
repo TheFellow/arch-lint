e45efc43e65e29ce54b3e6e08ac8a9932105e3f7
Ryan Harris <ryan.harris@strongdm.com>
2025-05-30 11:28:23 -0700

    pkg/linter: Ditch files completely in favor of imports only

--- main_test.go
+++ main_test.go
@@ -27,6 +27,6 @@ func TestArchLint(t *testing.T) {
 }
 
 var wantOut string = `
-arch-lint: [app package from api only] file "example/beta/bookstore/app/books/books.go" (package example/beta/bookstore/app/books) imports "example/beta/bookstore/app/authors"
-arch-lint: [app package from api or other features only] file "example/epsilon/bookstore/app/books/utils/bad.go" (package example/epsilon/bookstore/app/books/utils) imports "example/epsilon/bookstore/app/books"
-arch-lint: [no-experimental-imports] file "example/alpha/tester.go" (package example/alpha) imports "example/alpha/experimental"`
+arch-lint: [app package from api only] package "example/beta/bookstore/app/books" imports "example/beta/bookstore/app/authors"
+arch-lint: [app package from api or other features only] package "example/epsilon/bookstore/app/books/utils" imports "example/epsilon/bookstore/app/books"
+arch-lint: [no-experimental-imports] package "example/alpha" imports "example/alpha/experimental"`
--- pkg/linter/models_test.go
+++ pkg/linter/models_test.go
@@ -10,11 +10,10 @@ func TestViolation_String(t *testing.T) {
 	t.Parallel()
 	v := Violation{
 		Rule:    "my-rule",
-		File:    "foo.go",
 		Package: "path/to",
 		Import:  "path/to/bar",
 	}
 	got := v.String()
-	want := `arch-lint: [my-rule] file "foo.go" (package path/to) imports "path/to/bar"`
+	want := `arch-lint: [my-rule] package "path/to" imports "path/to/bar"`
 	testutil.Equals(t, got, want)
 }
